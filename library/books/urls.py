852997e3b97091ce30ac9d5ae28d964aad87a54c
Vladyslav Terebylo <terebylov@ssemi.cz>
2024-03-29 18:52:39 +0100

    Setted adminer, added articles to main page

--- library/books/urls.py
+++ library/books/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.show_main_page, name='show_main_page')
+    path('', views.show_main_page, name='show_main_page'),
+    path('post/<int:pk>/', views.show_post_by_id, name='show_post_by_id'),
 ]
--- library/books/views.py
+++ library/books/views.py
@@ -1,6 +1,13 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Book
 
 
 def show_main_page(request):
-    return HttpResponse('<h1>Main page</h1>')
+    library = Book.objects.all()
+    return render(request, 'books/mainpage.html', {'library': library})
+
+
+def show_post_by_id(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    return render(request, 'books/show_book_info.html', {'book': book})
